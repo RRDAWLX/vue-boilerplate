8e128dae0f5d97f3ae498cd74146bd5f4c2b081e
wanglongxiang <wanglongxiang02@meituan.com>
2018-10-08 18:31:33 +0800

    publish-tag 修改

--- services/publish-tag.js
+++ services/publish-tag.js
@@ -4,18 +4,26 @@ const execSync = require('child_process').execSync,
   Opts = {encoding: 'utf8'}
 
 let currentVersion = require('../package.json').version,
-  publishTags = execSync('git tag -l "v*" --sort=-refname', Opts).split('\n').filter(tag => {
-    return tag && execSync(`git cat-file -t ${tag}`, Opts).trim() === 'tag'
-  })
+  recentVersionTag = execSync('git tag -l "v*" --sort=-refname', Opts).trim().split(/\r?\n/)[0]
 
-if (publishTags.length) {   // 如果存在发布版本号
-  let lastPublishTag = execSync('git describe', Opts).trim()
+// 当前的版本号要大于最近一个版本号标签中的版本号
+if (recentVersionTag) {   // 如果存在版本号标签
+  let tagType = execSync(`git cat-file -t ${recentVersionTag}`, Opts).trim(),
+    recentVersionCommit
 
-  if (lastPublishTag !== publishTags[0]) {   // 且上一个版本号对应的 commit 不是当前的 commit
-    let lastPublishVersion = publishTags[0].slice(1)
+  if (tagType === 'tag') {
+    recentVersionCommit = execSync(`git cat-file -p ${recentVersionTag}`, Opts).slice(7, 14)
+  } else {
+    recentVersionCommit = execSync(`cat .git/refs/tags/${recentVersionTag}`, Opts).slice(0, 7)
+  }
 
-    if (semver.lte(currentVersion, lastPublishVersion)) {   // 则需校验当前的版本号是否大于上一个发布版本号
-      console.log(chalk.red(`当前版本号 ${currentVersion} 未大于上一个发布版本号 ${lastPublishVersion}，请升级版本号！`))
+  let currentCommit = execSync('git log --pretty=oneline', Opts).slice(0, 7)
+
+  if (recentVersionCommit !== currentCommit) {   // 且最近一个版本号标签对应的 commit 不是当前的 commit
+    let recentVersion = recentVersionTag.slice(1)
+
+    if (semver.lte(currentVersion, recentVersion)) {   // 则需校验当前 package.json 中的版本号是否大于上一个版本号
+      console.log(chalk.red(`当前版本号 ${currentVersion} 未大于上一个发布版本号 ${recentVersion}，请升级版本号！`))
       process.exit(1)
     }
   }
